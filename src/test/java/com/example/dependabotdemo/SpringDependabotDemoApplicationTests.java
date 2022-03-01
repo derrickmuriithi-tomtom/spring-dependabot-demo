92c26505d90ca80ded5f427b5c9813fccc2d4da9
muriithi <derrick.muriithi@tomtom.com>
2022-03-01 11:21:05 +0100

    add spring project

---
+++ src/test/java/com/example/dependabotdemo/SpringDependabotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependabotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDependabotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
